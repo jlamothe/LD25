3c27c0f65bdabb4383c9182f8070f842879437ad
Jonathan Lamothe <jonathan@jlamothe.net>
2012-12-15 12:13:34 -0500

    added object class

--- Types.hs
+++ Types.hs
@@ -15,9 +15,11 @@
 
 module Types
        ( GameState (..) 
+       , Object
        )
        where
 
+import Control.Monad
 import qualified Graphics.UI.SDL as SDL
 import qualified System.Random as Rand
 
@@ -27,3 +29,38 @@ data GameState =
   , audio :: Bool
   , gen :: Rand.StdGen
   } deriving Show
+
+class Object o where
+  getObjGeom :: o -> SDL.Rect
+  setObjGeom :: SDL.Rect -> o -> o
+  isObjVisible :: o -> Bool
+  objIsVisible :: o -> o
+  objIsInvisible :: o -> o
+  isObjEnabled :: o -> Bool
+  enableObj :: o -> o
+  disableObj :: o -> o
+  wasMouseOver :: o -> Bool
+  mouseWasOver :: o -> o
+  mouseWasNotOver :: o -> o
+  isObjPressed :: o -> Bool
+  objIsPressed :: o -> o
+  objNotPressed :: o -> o
+  onDrawObj :: SDL.Surface -> o -> IO ()
+  onMouseOver :: o -> IO o
+  onMouseOut :: o -> IO o
+  onPress :: o -> IO o
+  onRelease :: o -> IO o
+  onClick :: o -> IO o
+
+  onMouseOver = return
+  onMouseOut = return
+  onPress = return
+  onRelease = return
+  onClick = return
+
+drawObj :: Object o => SDL.Surface -> o -> IO Bool
+drawObj surf obj
+  | isObjVisible obj = onDrawObj surf obj >> return True
+  | otherwise = return False
+
+-- jl
